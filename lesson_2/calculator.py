2f56d5c547a77e4a07a3bf8285e637506a9bc1fe
Philip O'Connor <philoc1990@gmail.com>
2025-01-23 14:03:59 -0800

    changed the formatted string in calculator_messages and implemented the messaging system into calculator

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -5,6 +5,13 @@
 # Your calculator program is a hit, and it's being used all over the world! The problem is, not everyone speaks English. You now need to internationalize the messages in your calculator. You've already done the hard work of extracting all the messages to a configuration file. Now, all you have to do is send that configuration file to translators and call the right translation in your code.
 
 # Currently, our calculator only handles integer inputs. Let's modify it to accept floating-point numbers as well.
+
+# to access messages in a config file, we need to import json
+import json
+# since at least one message is printed outside of the main function, we open the JSON file here-- i don't really know what this does yet
+with open('calculator_messages.json', 'r') as file:
+    messages = json.load(file)
+
 def prompt(message):
     print(f"==> {message}")
 
@@ -16,28 +23,30 @@ def invalid_number(number_str):
 
     return False
 
-prompt('Welcome to Calculator!')
+# all messages are now written as "messages['key in JSON file']"
+prompt(messages['welcome'])
 
+# to allow the user to perform another calculation, we turn the main block of code into a function called main() so that we can have it call itself at the end.
 def main():
-    prompt("What's the first number?")
+    prompt(messages['first_number'])
     number1 = input()
 
     while invalid_number(number1):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(messages['invalid_number'])
         number1 = input()
 
-    prompt("What's the second number?")
+    prompt(messages['second_number'])
     number2 = input()
 
     while invalid_number(number2):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(messages['invalid_number'])
         number2 = input()
 
-    prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
+    prompt(messages['operation'])
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-        prompt("You must choose 1, 2, 3, or 4")
+        prompt(messages['invalid_operation'])
         operation = input()
 
     match operation:
@@ -50,11 +59,13 @@ def main():
         case "4":
             output = int(number1) / int(number2)
 
-    prompt(f"The result is {output}")
-    prompt("Would you like to perform another calculation? (y/n)")
+    # because this message was originally formatted, and I don't know how to mix formatted strings and JSON files, I turned output from an into to a str and then concatenated it to the JSON message
+    prompt(messages['result']+ str(output))
+    # here is where we ask the user if they'd like to perform another calculation.  we confirm the input, and if they said y then we invoke main() and start from the beginning.  If they say n, we don't do anything and the program terminates.
+    prompt(messages['new_calc'])
     answer = input()
     while answer != 'y' and answer != 'n':
-        prompt("You must answer y or n")
+        prompt(messages['invalid_answer'])
         answer = input()
     if answer == 'y':
         main()
