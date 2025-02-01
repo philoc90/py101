5edddc640fbabe0394a0366c9e2ada8787a23386
Philip O'Connor <philoc1990@gmail.com>
2025-02-01 10:43:09 -0800

    added the ability to have 0 APR

--- lesson_2/mortgage_calculator.py
+++ lesson_2/mortgage_calculator.py
@@ -1,6 +1,6 @@
 def invalid_amount(amount):
     try:
-        if float(amount) > 0:
+        if float(amount) >= 0:
             return False
         return True
     except ValueError:
@@ -15,6 +15,21 @@ def invalid_duration(duration):
     if int(duration) % 1 == 0 and int(duration) > 0:
         return False
     return True
+
+
+def calculate_payment(apr, loan_amount, number_of_months):
+    apr = float(apr)
+    loan_amount = float(loan_amount)
+    number_of_months = float(number_of_months)
+    monthly_rate = (apr / 12) * 0.01
+    try:
+        monthly_payment = loan_amount * (
+            monthly_rate / (1 - (1 + monthly_rate) ** (-number_of_months))
+        )
+    except ZeroDivisionError:
+        monthly_payment = loan_amount / number_of_months
+
+    return monthly_payment
 
 
 def main():
@@ -32,16 +47,15 @@ def main():
     while invalid_duration(number_of_months):
         number_of_months = input("Please input the duration in months: ")
 
-    monthly_rate = (float(apr) / 12) * 0.01
-    monthly_payment = float(loan_amount) * (
-        float(monthly_rate)
-        / (1 - (1 + float(monthly_rate)) ** (-float(number_of_months)))
-    )
+    monthly_payment = calculate_payment(apr, loan_amount, number_of_months)
 
     print(f"Your monthly payment is {monthly_payment:.2f}.")
-    print("Would you like to perform another calculation?  Enter y for yes, or anything else to exit.")
+    print(
+        "Would you like to perform another calculation?  Enter y for yes, or anything else to exit."
+    )
     answer = input().lower()
-    if answer == 'y':
+    if answer == "y":
         main()
+
 
 main()
