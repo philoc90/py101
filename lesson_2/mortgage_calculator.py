8843a2c109f7043fa9e83de9ed0aa7021c0d7128
Philip O'Connor <philoc1990@gmail.com>
2025-02-01 10:17:14 -0800

    added the abilty to do another calculation

--- lesson_2/mortgage_calculator.py
+++ lesson_2/mortgage_calculator.py
@@ -17,24 +17,31 @@ def invalid_duration(duration):
     return True
 
 
-print("Welcome to mortgage calculator!")
+def main():
+    print("Welcome to mortgage calculator!")
 
-loan_amount = input("Please input the loan amount:$")
-while invalid_amount(loan_amount):
-    loan_amount = input("Please input a positive number:$")
+    loan_amount = input("Please input the loan amount:$")
+    while invalid_amount(loan_amount):
+        loan_amount = input("Please input a positive number:$")
 
-apr = input("Please input the APR:%")
-while invalid_amount(apr):
-    apr = input("Please input a positive APR:%")
+    apr = input("Please input the APR:%")
+    while invalid_amount(apr):
+        apr = input("Please input a positive APR:%")
 
-number_of_months = input("Please input the loan's duration in months: ")
-while invalid_duration(number_of_months):
-    number_of_months = input("Please input the duration in months: ")
+    number_of_months = input("Please input the loan's duration in months: ")
+    while invalid_duration(number_of_months):
+        number_of_months = input("Please input the duration in months: ")
 
-monthly_rate = (float(apr) / 12) * 0.01
-monthly_payment = float(loan_amount) * (
-    float(monthly_rate)
-    / (1 - (1 + float(monthly_rate)) ** (-float(number_of_months)))
-)
+    monthly_rate = (float(apr) / 12) * 0.01
+    monthly_payment = float(loan_amount) * (
+        float(monthly_rate)
+        / (1 - (1 + float(monthly_rate)) ** (-float(number_of_months)))
+    )
 
-print(f"Your monthly payment is {monthly_payment:.2f}.")
+    print(f"Your monthly payment is {monthly_payment:.2f}.")
+    print("Would you like to perform another calculation?  Enter y for yes, or anything else to exit.")
+    answer = input().lower()
+    if answer == 'y':
+        main()
+
+main()
