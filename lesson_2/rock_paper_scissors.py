f38d4445aeedf33a791a03b66f3318c7080f0874
Philip O'Connor <philoc1990@gmail.com>
2025-02-07 12:02:11 -0800

    RPS game with all bonus features

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -2,6 +2,13 @@ import random
 
 
 VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"]
+WINNING_COMBOS = {
+    "rock": ["scissors", "lizard"],
+    "paper": ["rock", "spock"],
+    "scissors": ["paper", "lizard"],
+    "lizard": ["paper", "spock"],
+    "spock": ["scissors", "rock"],
+}
 
 
 def convert_choice(choice):
@@ -25,79 +32,73 @@ def prompt(message):
     print(f"==> {message}")
 
 
-def display_winner(choice, computer_choice):
-    prompt(f"You chose {choice}, computer chose {computer_choice}")
-    if (
-        (
-            choice == "rock"
-            and (computer_choice == "scissors" or computer_choice == "lizard")
-        )
-        or (
-            choice == "paper"
-            and (computer_choice == "rock" or computer_choice == "spock")
-        )
-        or (
-            choice == "scissors"
-            and (computer_choice == "paper" or computer_choice == "lizard")
-        )
-        or (
-            choice == "lizard"
-            and (computer_choice == "paper" or computer_choice == "spock")
-        )
-        or (
-            choice == "spock"
-            and (computer_choice == "scissors" or computer_choice == "rock")
-        )
-    ):
-        prompt("You win!")
-    elif (
-        (
-            choice == "rock"
-            and (computer_choice == "paper" or computer_choice == "spock")
-        )
-        or (
-            choice == "paper"
-            and (computer_choice == "scissors" or computer_choice == "lizard")
-        )
-        or (
-            choice == "scissors"
-            and (computer_choice == "rock" or computer_choice == "spock")
-        )
-        or (
-            choice == "lizard"
-            and (computer_choice == "rock" or computer_choice == "scissors")
-        )
-        or (
-            choice == "spock"
-            and (computer_choice == "lizard" or computer_choice == "paper")
-        )
-    ):
-        prompt("Computer wins!")
+def determine_round_winner(choice, computer_choice):
+    if computer_choice in WINNING_COMBOS[choice]:
+        round_winner = "player"
+    elif choice in WINNING_COMBOS[computer_choice]:
+        round_winner = "computer"
     else:
-        prompt("It's a tie!")
+        round_winner = "nobody"
+    return round_winner
 
 
-keep_going = True
-while keep_going:
-    prompt(
-        f"Choose one: {', '.join(VALID_CHOICES)}. (you may type the first letter of any choice instead.  Use s for spock and c for scissors.)"
-    )
-    choice = convert_choice(input().lower())
+def display_round_winner(round_winner, choice, computer_choice):
+    prompt(f"You chose {choice}, computer chose {computer_choice}")
+    match round_winner:
+        case "player":
+            prompt("You win the round!")
+        case "computer":
+            prompt("Computer wins the round!")
+        case "nobody":
+            prompt("This round is a tie!")
 
-    while choice not in VALID_CHOICES:
-        prompt("That's not a valid choice")
-        choice = convert_choice(input().lower())
 
-    computer_choice = random.choice(VALID_CHOICES)
+def determine_scores(round_winner, scores):
+    match round_winner:
+        case "player":
+            scores[0] += 1
+        case "computer":
+            scores[1] += 1
+    return scores
 
-    display_winner(choice, computer_choice)
 
-    while True:
-        prompt("Do you want to play again (y/n)?")
-        answer = input().lower()
-        if answer.startswith("n") or answer.startswith("y"):
-            break
-        else:
+def display_overall_winner(scores):
+    if scores[0] == 3:
+        prompt("Player wins the match!")
+    else:
+        prompt("Computer wins the match!")
+
+
+def main():
+    keep_going = True
+    while keep_going:
+        scores = [0, 0]
+        while scores[0] != 3 and scores[1] != 3:
+            prompt(f"Choose one: {', '.join(VALID_CHOICES)}.")
+            choice = convert_choice(input().lower())
+
+            while choice not in VALID_CHOICES:
+                prompt("That's not a valid choice")
+                choice = convert_choice(input().lower())
+
+            computer_choice = random.choice(VALID_CHOICES)
+            round_winner = determine_round_winner(choice, computer_choice)
+            display_round_winner(round_winner, choice, computer_choice)
+
+            scores = determine_scores(round_winner, scores)
+            prompt(
+                f"The score is now player: {scores[0]}, computer: {scores[1]}."
+            )
+        display_overall_winner(scores)
+
+        while True:
+            prompt("Do you want to play again (y/n)?")
+            answer = input().lower()
+            if answer.startswith("n") or answer.startswith("y"):
+                break
             prompt("That's not a valid choice")
-    if answer[0] == "n":
-        keep_going = False
+        if answer[0] == "n":
+            keep_going = False
+
+
+main()
