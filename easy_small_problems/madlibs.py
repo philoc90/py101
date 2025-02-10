66ffe060b412a4c137bc74aa38bb414160c5afa8
Philip O'Connor <philoc1990@gmail.com>
2025-02-10 11:17:41 -0800

    madlibs

---
+++ easy_small_problems/madlibs.py
@@ -0,0 +1,10 @@
+# Create a simple madlib program that prompts for a noun, a verb, an adverb, and an adjective, and injects them into a story that you create.
+
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+adverb = input("Enter an adverb: ")
+
+print(f"Did you {verb} your {adjective} {noun} {adverb}?  That's hilarious!")
+print(f"The {adjective} {noun} {verb}s {adverb} over the lazy dog.")
+print(f"The {noun} {adverb} {verb}s up to Joe's {adjective} turtle.")
